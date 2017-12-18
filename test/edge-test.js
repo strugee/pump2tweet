96ac3da04f7946ecd7b606602ff4f088820a3cba
AJ Jordan <alex@strugee.net>
2017-12-17 19:09:37 -0500

    Add unit tests for all the models
    These are almost useless, as they really only catch *massive* blowups
    like syntax errors. However, the fact that these modules are loaded at
    all in the test environment means that they're tracked for code
    coverage. And that's a big win.
    
    Ref #10

---
+++ test/edge-test.js
@@ -0,0 +1,40 @@
+// test the edge model module
+//
+// Copyright 2017 AJ Jordan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var vows = require("perjury"),
+    assert = vows.assert,
+    databank = require("databank");
+
+// TODO expand this file
+
+vows.describe("Edge model").addBatch({
+	"When we require the module": {
+		topic: function() {
+			return require("../models/edge");
+		},
+		"it works": function(err) {
+			assert.ifError(err);
+		},
+		"it exports a DatabankObject": function(err, Edge) {
+			assert.isObject(Edge);
+			// Duck typing ftw
+			assert.isFunction(Edge.bank);
+		},
+		"it has a schema": function(err, Edge) {
+			assert.isObject(Edge.schema);
+		}
+	}
+}).export(module);
